587cf995164a31bfd656abf135dcd8cd309e56d4
NguyenHai1449 <nguyenhai1449@gmail.com>
2022-10-03 18:03:34 +0700

    add controller

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,17 +1,14 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-
-enum GenderEnum {
-    female = 'female',
-    male = 'male',
-    other = 'other',
-}
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface IFormInput {
     firstName: string;
     LastName: string;
     email: string;
+    birthday: Date;
     password: string;
     confirmPassword: string;
     isAgree: boolean;
@@ -19,17 +16,18 @@ interface IFormInput {
 
 const schema = yup
     .object({
-        firstName: yup.string().required(),
-        LastName: yup.string().required(),
-        email: yup.string().required(),
-        password: yup.string().required(),
-        confirmPassword: yup.string().required(),
+        firstName: yup.string().required('First name is a required field'),
+        LastName: yup.string().required('Last name is a required field'),
+        email: yup.string().required('Email is a required field'),
+        password: yup.string().required('Password is a required field'),
+        confirmPassword: yup.string().required('Confirm password is a required field'),
     })
     .required();
 
 const RegisterForm = () => {
     const {
         register,
+        control,
         handleSubmit,
         formState: { errors },
     } = useForm<IFormInput>({
@@ -76,6 +74,25 @@ const RegisterForm = () => {
                 <span className="text-red-100">{errors.email?.message}</span>
             </div>
 
+            <div className="mb-6">
+                <label htmlFor="birthday">Birthday</label>
+                <Controller
+                    name="birthday"
+                    control={control}
+                    render={({ field: { value, ...fieldProps } }) => (
+                        <ReactDatePicker
+                            {...fieldProps}
+                            placeholderText="Select date"
+                            selected={value}
+                            autoComplete="off"
+                            className="h-14 block w-full border border-gray-200"
+                        />
+                    )}
+                />
+
+                <span className="text-red-100">{errors.birthday?.message}</span>
+            </div>
+
             <div className="grid gap-6 md:grid-cols-2 mb-6">
                 <div>
                     <label htmlFor="password">Password</label>
@@ -101,7 +118,11 @@ const RegisterForm = () => {
 
             <div className="mb-6">
                 <div>
-                    <input id="remember" type="checkbox" {...register('isAgree')}></input>
+                    <input
+                        id="remember"
+                        type="checkbox"
+                        {...register('isAgree', { required: true })}
+                    ></input>
                     <label htmlFor="remember" className="ml-2 text-md font-medium text-gray-900">
                         I agree to the{' '}
                         <span className="text-blue-600 hover:underline">Terms and Conditions</span>.
@@ -111,7 +132,7 @@ const RegisterForm = () => {
             </div>
 
             <input
-                className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-10 py-2.5 text-center cursor-pointer"
+                className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-10 py-2.5 text-center"
                 type="submit"
                 value="Register"
             />
